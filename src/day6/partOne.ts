e525759dc0e25b04b22c4db506793319351a3531
Viktor Kushnir <viktor.kushnir@djangostars.com>
2023-12-12 18:29:27 +0200

    add day six solution

--- src/day6/partOne.ts
+++ src/day6/partOne.ts
@@ -1,10 +1,46 @@
 import { PathLike } from 'fs'
 import { readInput } from '../day2'
 
-export default function partOne(input: PathLike): number {
-  const rawLines = readInput(input)
-  console.log(rawLines)
-  return 0
+export interface Race {
+  time: number
+  distRecord: number
 }
 
-partOne('/home/codelance/projects/aoc/src/tests/day6/mockups/input.txt')
+export function getNumbers(line: string): number[] {
+  return line.split(':')[1].trim().split(' ').map((e) => Number(e)).filter(Boolean)
+}
+
+export function getRaces(times: number[], records: number[]): Race[] {
+  return times.reduce<Race[]>((record, time:number, index:number) => {
+    return [
+      ...record,
+      {
+        time, distRecord: records[index],
+      },
+    ]
+  }, [])
+}
+
+export default function partOne(input: PathLike): number {
+  const [
+    raceTimes,
+    raceRecords,
+  ] = readInput(input).map(getNumbers)
+
+  const races: Race[] = getRaces(raceTimes, raceRecords)
+  const results = races.reduce<number[][]>((res, race) => {
+    return [
+      ...res,
+      new Array(Math.ceil(race.time)).fill(1).map((x, idx) => x + idx).filter((holdValue) => {
+        return holdValue < race.time && holdValue * ( race.time - holdValue ) > race.distRecord
+      }
+
+      ),
+    ]
+  }, [])
+
+  return results.reduce<number>((multiply, race) => {
+    return multiply * race.length
+  }, 1)
+}
+
--- src/day6/partTwo.ts
+++ src/day6/partTwo.ts
@@ -1,7 +1,26 @@
 import { PathLike } from 'fs'
 import { readInput } from '../day2'
+import { Race } from './partOne'
+
+function getNumber(line: string): number {
+  return Number(line.split(':')[1].trim().split(' ').filter(Boolean).join(''))
+}
+
+function getRace(time: number, record: number): Race {
+  return {
+    time,
+    distRecord: record,
+  }
+}
 
 export default function partTwo(input: PathLike): number {
-  const rawLines = readInput(input)
-  return 0
+  const [
+    time,
+    record,
+  ] = readInput(input).map(getNumber)
+  const race = getRace(time, record)
+  const results = new Array(Math.floor(race.time / 2)).fill(1).map((x, idx) => x + idx).filter((holdValue) => {
+    return holdValue < race.time && holdValue * (race.time - holdValue) > race.distRecord
+  })
+  return results.length * 2
 }
